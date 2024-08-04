9a9b5b7d4ad837160a2efcdd00c98e9a8b7e9075
Felipe Lube de Bragança <felipe@studentfinance.com>
2024-08-04 20:43:04 -0300

    fix: logout on error

--- src/components/hoc/withAuthErrorHandling.tsx
+++ src/components/hoc/withAuthErrorHandling.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import { Navigate, useRouteError } from "react-router-dom";
 import { ROUTES } from "../../config/routes";
 import { ApiError } from "../../services/ApiService";
+import { useAuth } from "../providers/AuthProvider";
 
 const withAuthErrorHandling = <P extends object>(
   WrappedComponent?: React.ComponentType<P>
 ) => {
   const ComponentWithAuthErrorHandling: React.FC<P> = (props) => {
     const apiError = (useRouteError() as ApiError) ?? {};
+    const { logout } = useAuth();
 
     if (
       apiError &&
       (apiError.statusCode === 401 || apiError.statusCode === 403)
     ) {
+      logout();
       return <Navigate to={ROUTES.signIn} />;
     }
 
