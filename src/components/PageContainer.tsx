410284d0c2a0471bbe5f8fc2e4e3de0771de154c
Felipe Lube de Bragança <felipe@studentfinance.com>
2024-08-04 17:44:43 -0300

    fix: limit PageContent height

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -1,10 +1,12 @@
 export type PageContentProps = {} & React.HTMLAttributes<HTMLDivElement>;
 
-export function PageContent({ ...props }: PageContentProps) {
+export function PageContent({ children, ...props }: PageContentProps) {
   return (
     <div
       {...props}
-      className={`w-full bg-white p-4 rounded-2xl rounded-b-none flex flex-col gap-4 ${props.className}`}
-    ></div>
+      className={`w-full bg-white p-4 rounded-2xl rounded-b-none flex flex-col gap-4 max-h-[85vh] overflow-scroll ${props.className}`}
+    >
+      {children}
+    </div>
   );
 }
